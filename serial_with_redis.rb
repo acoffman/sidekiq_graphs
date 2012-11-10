d834ee82087f6ba70aa5810d9ebf2811c2fe770d
Adam Coffman <acoffman@genome.wustl.edu>
2012-11-10 11:00:28 -0600

    unscientific timing stuff

--- serial_with_redis.rb
+++ serial_with_redis.rb
@@ -3,14 +3,18 @@ require '~/git/sidekiq_testing/redis_graph'
 require '~/git/sidekiq_testing/graph'
 require '~/git/sidekiq_testing/extensions/string'
 
-number_of_nodes = ARGV[0].to_i
 
-g = Graph.new
+[10, 50, 100, 500, 1000, 5000, 10_000, 50_000, 100_000, 500_000].each do |num|
+  t = Time.now
+  g = Graph.new
 
-n1 = g.create_node( 'PrintNode', 0 )
-(1..number_of_nodes).each do |node_num|
-  n2 = g.create_node( 'PrintNode', node_num )
-  g.link( n1, n2 )
-  n1 = n2
+  n1 = g.create_node( 'PrintNode', 0 )
+  (1..num).each do |node_num|
+    n2 = g.create_node( 'PrintNode', node_num )
+    g.link( n1, n2 )
+    n1 = n2
+  end
+  RedisGraph.store(g)
+  puts "#{num}\t#{Time.now - t}"
 end
-RedisGraph.store(g)
+
